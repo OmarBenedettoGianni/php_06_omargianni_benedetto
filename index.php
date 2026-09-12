5164d2d82f7027bdcf6290e482eba9f9f2e197bc
OmarBenedettoGianni <omarbenedetto200012@gmail.com>
2026-09-12 17:37:27 +0200

    creato switch per numeri 1 a 100

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php
+
+for($i=1;$i<=100;$i++){
+    switch($i){
+        case $i % 15 == 0:
+           echo "HACKCADEMY \n";
+            break;
+        case $i % 3 == 0:
+           echo "PHP \n";
+            break;
+        case $i % 5 == 0:
+           echo "JAVASCRIPT \n";
+            break;
+    default:
+        echo "$i \n";
+        break;
+    }
+};
